feccdcd1d3585bb3bd0ae58bb8805673fddd7edf
ezeematias <eunia69@gmail.com>
2021-03-19 16:39:23 -0300

    Ejercicio 04

--- Ejercicio04/Program.cs
+++ Ejercicio04/Program.cs
@@ -10,6 +10,29 @@ namespace Ejercicio04
     {
         static void Main(string[] args)
         {
+            int perfectNumber = 6;
+            int collector = 0;
+            int amount = 0;
+            Console.WriteLine("Los primeros cuatro números perfectos son: ");
+            do
+            {
+                for(int i = 1; i < perfectNumber; i++)
+                {
+                    if (perfectNumber % i == 0)
+                    {
+                        collector += i;
+                    }
+                }
+                if (perfectNumber == collector)
+                {
+                    Console.WriteLine(perfectNumber);
+                    amount++;
+                }
+                collector = 0;
+                perfectNumber++;
+            } while (amount < 4);
+            Console.WriteLine("\n\nFIN =)");
+            Console.ReadKey();
 
 
         }
