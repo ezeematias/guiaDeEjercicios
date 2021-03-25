baec6fa81e36dc1f2b0d93b583de4853564fa0af
ezeematias <eunia69@gmai.com>
2021-03-24 23:19:45 -0300

    Intento del Ejercicio 19

---
+++ Biblioteca/Sumador.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Sumador
+    {
+        public int cantidadSumas;
+
+        public Sumador(int num)
+        {
+            this.cantidadSumas = num;
+        }
+        public Sumador()
+            : this(0)
+        {
+
+        }
+
+        public long Sumar(long a, long b)
+        {
+            this.cantidadSumas++;
+            return a + b;
+        }
+
+        public string Sumar(string a, string b)
+        {
+            this.cantidadSumas++;
+            return a + " " + b;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
+        public long Sumar(long a, long b)
+        {
+            this.cantidadSumas++;
+            return a + b;
+        }
+
+        public string Sumar(string a, string b)
+        {
+            return a + " " + b;
+        }
+
+        public static explicit operator int(Sumador sum)
+        {
+            return sum.cantidadSumas;
+        }
+
+        public static long operator + (Sumador s1, Sumador s2)
+        {
+            return s1.cantidadSumas + s2.cantidadSumas;
+        }
+
+        public static bool operator | (Sumador s1, Sumador s2)
+        {
+            return s1.cantidadSumas == s2.cantidadSumas;
+        }
+        */
+
+
+    }
+}
---
+++ Ejercicio19/Program.cs
@@ -0,0 +1,21 @@
+﻿using Biblioteca;
+using System;
+
+namespace Ejercicio19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long suma;
+            Sumador s1 = new Sumador();
+            suma = s1.Sumar(10, 20);
+
+            Console.WriteLine("SUMADOR S1 " + s1 + "\nRESULTADO SUMA " + suma + "\nCANTIDAD SUMAS " + s1.cantidadSumas);
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
